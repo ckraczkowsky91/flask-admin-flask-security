60193d8be3c5962c112d3367c0f5dc6f4856a3d9
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-01-25 17:44:01 -0800

    Installed bcrypt and dependent packages.

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore
+from flask_security import Security, SQLAlchemyUserDatastore, login_required, UserMixin
 
 # Instantiate the Flask application with configurations
 secureApp = Flask(__name__)
 secureApp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 secureApp.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask-admin-flask-security-db'
+secureApp.config['SECRET_KEY'] = 'secretkey'
+secureApp.config['SECURITY_PASSWORD_SALT'] = 'none'
 
 # Instantiate the database
 db = SQLAlchemy(secureApp)
 
 # Create a table of users and user roles
 roles_users_table = db.Table('roles_users',
-                            db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                            db.Column('people_id', db.Integer(), db.ForeignKey('people.id')),
                             db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
 
 # Define models for the users and user roles
@@ -21,17 +23,17 @@ class Role(db.Model):
     name = db.Column(db.String(80), unique=True)
     description = db.Column(db.String(255))
 
-class User(db.Model):
+class People(db.Model, UserMixin):
     id = db.Column(db.Integer(), primary_key=True)
     email = db.Column(db.String(255), unique=True)
     password = db.Column(db.String(80))
     active = db.Column(db.Boolean())
     confirmed_at = db.Column(db.DateTime())
 
-    roles = db.relationship('Roles', secondary=roles_users_table)
+    roles = db.relationship('Role', secondary=roles_users_table)
 
 # Create a datastore and instantiate Flask-Security
-user_datastore = SQLAlchemyUserDatastore(db, User, Role)
+user_datastore = SQLAlchemyUserDatastore(db, People, Role)
 security = Security(secureApp, user_datastore)
 
 # Create the tables for the users and roles and add a user to the user table
@@ -39,10 +41,13 @@ security = Security(secureApp, user_datastore)
 #  i.e. calling localhost:5000 from the browser
 @secureApp.before_first_request
 def create_user():
+    # db.drop_all()
     db.create_all()
     user_datastore.create_user(email='admin', password='admin')
+    db.session.commit()
 
 # Define the index route
 @secureApp.route('/')
+@login_required
 def index():
     return '<h1>Hey</h1>'
