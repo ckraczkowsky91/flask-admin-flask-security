435e30d2bd3295851d389318bc737f19272f699f
Colin Thomas Kraczkowsky <Home@Colins-MacBook-Pro-2.local>
2020-01-26 20:55:14 -0800

    Added two views to override the built-in base template and index template for flask-admin.

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, url_for
 from flask_admin import Admin
+from flask_admin import helpers as admin_helpers
+from flask_admin.contrib.sqla import ModelView
 from flask_sqlalchemy import SQLAlchemy
-from flask_security import Security, SQLAlchemyUserDatastore, login_required, UserMixin
+from flask_security import current_user, Security, SQLAlchemyUserDatastore, login_required, UserMixin
 
 # Instantiate the Flask application with configurations
 secureApp = Flask(__name__)
-secureApp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-secureApp.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask-admin-flask-security-db'
+secureApp.config['FLASK_ADMIN_SWATCH'] = 'sandstone'
 secureApp.config['SECRET_KEY'] = 'secretkey'
 secureApp.config['SECURITY_PASSWORD_SALT'] = 'none'
+secureApp.config['SECURITY_REGISTERABLE'] = True
+secureApp.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+secureApp.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://localhost/flask-admin-flask-security-db'
 
 # Instantiate the database
 db = SQLAlchemy(secureApp)
@@ -42,19 +46,38 @@ security = Security(secureApp, user_datastore)
 #  i.e. calling localhost:5000 from the browser
 @secureApp.before_first_request
 def create_user():
+    db.drop_all()
     db.create_all()
     user_datastore.create_user(email='admin', password='admin')
     db.session.commit()
 
 # Instantiate Flask-Admin
-admin = Admin(secureApp, name='Admin', template_mode='bootstrap3')
+admin = Admin(secureApp, name='Admin', base_template='my_master.html', template_mode='bootstrap3')
 
 # Create a ModelView to add to our administrative interface
+class UserModelView(ModelView):
+    def is_accessible(self):
+        return (current_user.is_active and
+                current_user.is_authenticated)
+
+    def _handle_view(self, name):
+        if not self.is_accessible():
+            return redirect(url_for('security.login'))
 
 # Add administrative views to Flask-Admin
+admin.add_view(UserModelView(Users, db.session))
+
+# Define a context processor for merging Flask-Admin's template context into the Flask-Security views
+@security.context_processor
+def security_context_processor():
+    return dict(
+        admin_base_template = admin.base_template,
+        admin_view = admin.index_view,
+        h = admin_helpers,
+        get_url = url_for
+    )
 
 # Define the index route
 @secureApp.route('/')
-@login_required
 def index():
     return render_template('index.html')
